0f88da90d9f391c180a1b71340719cd1d1b342f7
binh.tran <binh.tran@codeenginestudio.com>
2020-08-27 20:21:56 +0700

    Init portlet mvc

---
+++ modules/amf-registration/amf-registration-web/src/main/java/com/amf/registration/constants/AMFRegistrationPortletKeys.java
@@ -0,0 +1,11 @@
+package com.amf.registration.constants;
+
+/**
+ * @author ces-user
+ */
+public class AMFRegistrationPortletKeys {
+
+	public static final String AMFREGISTRATION =
+		"com_amf_registration_AMFRegistrationPortlet";
+
+}
